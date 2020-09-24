b8ba9c8c1d8ff47ce90f85071b340e23b274a54a
Maya Benowitz <mayabenowitz@gmail.com>
2020-09-23 19:34:15 -0500

    lets do this

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Free Massively Multiplayer Online Learning Platform',
+    author='maya benowitz',
+    license='MIT',
+)
